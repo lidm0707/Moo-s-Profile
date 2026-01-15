8c0cd50890f1350d43f74d2f0796a5c1c5ca0e33
lidm0707 <lightdemon17@gmail.com>
2026-01-15 20:31:15 +0700

    add picture

--- src/components/header.rs
+++ src/components/header.rs
@@ -9,11 +9,31 @@ pub fn Header() -> Element {
     rsx! {
         header {
             class: if dark_mode() { "profile-header" } else { "profile-header light-mode" },
+            style: format!(
+                "background-image:
+                linear-gradient(
+                  rgba(10, 10, 20, 0.85),
+                  rgba(10, 10, 20, 0.85)
+                ),url({});",
+                asset!("/assets/bg_profile.jpg")
+            ),
             div {
-                class: "profile-info",
-                h1 { "Hi there 👋 I'm Moo!" }
-                p { "I'm passionate about creating, learning, and innovating in the field of technology." }
-                p { "I'm from Thailand, and I love diving into new challenges and opportunities." }
+                class: "profile-info ",
+                div{
+                    img {
+                    src: asset!("/assets/profile.jpg"),
+                    alt: "Moo's profile picture",
+                    class: "profile-image",
+                }
+                }
+                div{class:"glow-text",
+                    h1 { "Hi there I'm Moo!" }
+                    p { "I'm passionate about creating, learning, and innovating in the field of technology." }
+                    p { "I'm from Thailand, and I love diving into new challenges and opportunities." }
+                }
+
+
+
 
                 // Social links
                 div {
